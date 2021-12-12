b9fcd8e25559546fba843deb2e087beba3016875
Daniel Qiu <65613404+danielqiuu@users.noreply.github.com>
2021-12-12 14:38:36 -0500

    Updated
    Loops through articles in a directory and provides a sentiment analysis for each txt file / month

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -1,12 +1,12 @@
+import os
+import ast
 import nltk
 from nltk.sentiment import SentimentIntensityAnalyzer
 
 sia = SentimentIntensityAnalyzer()
-contents = ['Schools Are Closing Classrooms on Fridays. Parents Are Furious. Desperate to keep teachers, some districts have turned to remote teaching for one day a week — and sometimes more. Families have been left to find child care.',
-    'There have already been successful transitions amongst many universities. For example, Zhejiang University managed to get more than 5,000 courses online just two weeks into the transition using “DingTalk ZJU”. The Imperial College London started offering a course on the science of coronavirus, which is now the most enrolled class launched in 2020 on Coursera.'
-]
 
-def analyze_sentiment(content: str)  -> float:
+
+def analyze_sentiment(content: str) -> float:
     """
     Function that takes in content (usually paragraphs) and returns an overall sentiment score using the VADER model.
     Returns a float rounded to 3 decimals ranging from -1, to 1
@@ -18,11 +18,29 @@ def analyze_sentiment(content: str)  -> float:
         score = sia.polarity_scores(sentence)
         polarity_sum += score['compound']
         # print(score)
-    overall_score = polarity_sum / len(sentences)
-    return round(overall_score, 3)
-    # TODO: Can return a tuple with % score and whether it is positive or negative. i.e("positive", 86)
+    overall_score = (polarity_sum / len(sentences))
+    return overall_score
 
 
-# Example usage
-for content in contents:
-    print(analyze_sentiment(content))
+def month_sentiment() -> list[float]:
+    """
+    Returns a list of sentiment scores.
+    """
+    scores = []
+    path_of_directory = 'processed_articles'
+    articles_in_folder = os.listdir(path_of_directory)  # list of all the processed articles
+    for file_name in articles_in_folder:
+        articles_file = open(path_of_directory + '/' + file_name, 'r')
+        articles = ast.literal_eval(
+            articles_file.read())  # read the text inside the file and convert it to a dictionary
+        sentiment_sum = 0
+        content_counter = 0
+        for date in articles:
+
+            if articles[date] != []:
+                sentiment_sum += analyze_sentiment(articles[date][0])
+                content_counter += 1
+        total_sentiment = round(((sentiment_sum / content_counter) * 100), 2)
+        scores.append(total_sentiment)
+    return scores
+
